988092d4ae6ae51dccd38b50b4752a6380973f35
dacoolguy1 <emmanueltemiede@gmail.com>
2025-02-25 11:01:11 +0100

    updated

--- lib/view/intro/components/description_text.dart
+++ lib/view/intro/components/description_text.dart
@@ -15,7 +15,7 @@ class AnimatedDescriptionText extends StatelessWidget {
       duration: const Duration(milliseconds: 200),
       builder: (context, value, child) {
         return Text(
-          kIsWeb
+          !Responsive.isMobile(context)
               ? 'I\'m capable of creating excellent mobile apps, handling ${Responsive.isLargeMobile(context) ? '\n' : ''}every step from ${!Responsive.isLargeMobile(context) ? '\n' : ''}concept to deployment with over 3 years of industry experience building ${!Responsive.isLargeMobile(context) ? '\n' : ''}quality mobile apps for clients.'
               : 'I\'m capable of creating excellent mobile apps, handling ${Responsive.isLargeMobile(context) ? '\n' : ''}every step from ${!Responsive.isLargeMobile(context) ? '\n' : ''}concept to deployment',
           maxLines: 3,
